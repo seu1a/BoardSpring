ada6f3faeb21f9d9b5b68951930034d1bb04f214
이슬아 <rheeseula@gmail.com>
2024-03-12 23:11:41 +0900

    Feat :: BoardController 생성 (Adapter - In - Web)

---
+++ src/main/java/kr/seula/boardspring/adapter/in/web/BoardController.java
@@ -0,0 +1,10 @@
+package kr.seula.boardspring.adapter.in.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("board")
+public class BoardController {
+
+}
