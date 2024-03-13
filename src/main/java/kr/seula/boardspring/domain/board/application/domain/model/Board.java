a7e3243a9fad0cdfaa80e2a7d2c2f6f80b57c1de
이슬아 <rheeseula@gmail.com>
2024-03-14 08:45:08 +0900

    Feat :: Board 도메인 BoardDescription 필드 추가 (Application - Domain - Model)

--- src/main/java/kr/seula/boardspring/domain/board/application/domain/model/Board.java
+++ src/main/java/kr/seula/boardspring/domain/board/application/domain/model/Board.java
@@ -1,6 +1,8 @@
 package kr.seula.boardspring.domain.board.application.domain.model;
 
+import kr.seula.boardspring.domain.board.application.domain.model.value.BoardDescription;
 import kr.seula.boardspring.domain.board.application.domain.model.value.BoardId;
+import kr.seula.boardspring.domain.board.application.domain.model.value.BoardTitle;
 import lombok.Getter;
 
 @Getter
@@ -8,6 +10,8 @@ public class Board {
 
     private BoardId id;
 
-    private Title title;
+    private BoardTitle title;
+
+    private BoardDescription description;
 
 }
