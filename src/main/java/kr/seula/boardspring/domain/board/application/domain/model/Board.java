859ce080a3eee326656577dae630b49195cca14a
이슬아 <rheeseula@gmail.com>
2024-03-14 08:27:26 +0900

    Feat :: Board 도메인 Username 필드 추가 (Application - Domain - Model)

--- src/main/java/kr/seula/boardspring/domain/board/application/domain/model/Board.java
+++ src/main/java/kr/seula/boardspring/domain/board/application/domain/model/Board.java
@@ -1,11 +1,14 @@
 package kr.seula.boardspring.domain.board.application.domain.model;
 
 import kr.seula.boardspring.domain.board.application.domain.model.value.UserID;
+import kr.seula.boardspring.domain.board.application.domain.model.value.Username;
 import lombok.Getter;
 
 @Getter
 public class Board {
 
     private UserID id;
+
+    private Username username;
 
 }
