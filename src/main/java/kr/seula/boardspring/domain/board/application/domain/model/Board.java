65b89224419073e80b7edc529870ca7e6437431c
이슬아 <rheeseula@gmail.com>
2024-03-14 08:41:08 +0900

    Feat :: Board 도메인 BoardTitle 필드 추가 (Application - Domain - Model)

--- src/main/java/kr/seula/boardspring/domain/board/application/domain/model/Board.java
+++ src/main/java/kr/seula/boardspring/domain/board/application/domain/model/Board.java
@@ -1,11 +1,13 @@
 package kr.seula.boardspring.domain.board.application.domain.model;
 
-import kr.seula.boardspring.domain.board.application.domain.model.value.UserID;
+import kr.seula.boardspring.domain.board.application.domain.model.value.BoardId;
 import lombok.Getter;
 
 @Getter
 public class Board {
 
-    private UserID id;
+    private BoardId id;
+
+    private Title title;
 
 }
