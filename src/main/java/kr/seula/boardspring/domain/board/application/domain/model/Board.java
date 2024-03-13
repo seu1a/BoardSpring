ac69ac7095b653f3bbee75475d5206822b1a884d
이슬아 <rheeseula@gmail.com>
2024-03-14 08:34:07 +0900

    Feat :: Board 도메인에 왜 추가했지 ??

--- src/main/java/kr/seula/boardspring/domain/board/application/domain/model/Board.java
+++ src/main/java/kr/seula/boardspring/domain/board/application/domain/model/Board.java
@@ -1,17 +1,11 @@
 package kr.seula.boardspring.domain.board.application.domain.model;
 
-import kr.seula.boardspring.domain.board.application.domain.model.value.Password;
 import kr.seula.boardspring.domain.board.application.domain.model.value.UserID;
-import kr.seula.boardspring.domain.board.application.domain.model.value.Username;
 import lombok.Getter;
 
 @Getter
 public class Board {
 
     private UserID id;
-
-    private Username username;
-
-    private Password password;
 
 }
