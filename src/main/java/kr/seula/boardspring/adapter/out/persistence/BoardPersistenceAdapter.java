7461659b01e12155f1fd61b91aaa8fa16d091e97
이슬아 <rheeseula@gmail.com>
2024-03-13 23:13:30 +0900

    Feat :: BoardPersistenceAdapter 이동 (Domain - Board - Adapter - Out - Persistence)

--- src/main/java/kr/seula/boardspring/adapter/out/persistence/BoardPersistenceAdapter.java
+++
@@ -1,6 +0,0 @@
-package kr.seula.boardspring.adapter.out.persistence;
-
-import kr.seula.boardspring.port.out.CreateBoardUseCase;
-
-public class BoardPersistenceAdapter extends CreateBoardUseCase {
-}
---
+++ src/main/java/kr/seula/boardspring/domain/board/adapter/out/persistence/BoardPersistenceAdapter.java
@@ -0,0 +1,9 @@
+package kr.seula.boardspring.domain.board.adapter.out.persistence;
+
+import kr.seula.boardspring.domain.board.application.port.out.CreateBoardPort;
+
+public class BoardPersistenceAdapter implements CreateBoardPort {
+
+
+
+}
