092f7a085b2cb90659a60345545304bef914519e
이슬아 <rheeseula@gmail.com>
2024-03-14 14:52:52 +0900

    Feat :: BoardEntity title, description 필드 추가 (Adapter - Out - Persistence - Entity)

--- src/main/java/kr/seula/boardspring/domain/board/adapter/out/persistence/entity/BoardEntity.java
+++ src/main/java/kr/seula/boardspring/domain/board/adapter/out/persistence/entity/BoardEntity.java
@@ -1,15 +1,33 @@
 package kr.seula.boardspring.domain.board.adapter.out.persistence.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
+@NoArgsConstructor
 public class BoardEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Builder
+    public BoardEntity(
+            Long id,
+            String title,
+            String description
+    ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
 
 }
