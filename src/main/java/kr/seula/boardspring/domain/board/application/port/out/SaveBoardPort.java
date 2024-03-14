cd4c9570226b9290a74c949f9c77e307937dd21d
이슬아 <rheeseula@gmail.com>
2024-03-14 14:51:47 +0900

    Feat :: SaveBoardPort saveBoard 메서드 추가 (Application - Port - Out)

--- src/main/java/kr/seula/boardspring/domain/board/application/port/out/SaveBoardPort.java
+++ src/main/java/kr/seula/boardspring/domain/board/application/port/out/SaveBoardPort.java
@@ -1,7 +1,9 @@
 package kr.seula.boardspring.domain.board.application.port.out;
 
+import kr.seula.boardspring.domain.board.application.domain.model.Board;
+
 public interface SaveBoardPort {
 
-
+    void saveBoard(Board board);
 
 }
