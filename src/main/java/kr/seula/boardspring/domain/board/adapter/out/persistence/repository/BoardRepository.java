8e0d33cb5d02808efc6ca763be71664edf5ec8b6
이슬아 <rheeseula@gmail.com>
2024-03-14 14:50:25 +0900

    Feat :: Board 도메인 Repository save 메서드 추가 (Adapter - Out- Persistence - Repository)

--- src/main/java/kr/seula/boardspring/domain/board/adapter/out/persistence/repository/BoardRepository.java
+++ src/main/java/kr/seula/boardspring/domain/board/adapter/out/persistence/repository/BoardRepository.java
@@ -1,4 +1,9 @@
 package kr.seula.boardspring.domain.board.adapter.out.persistence.repository;
 
-public class BoardRepository {
+import kr.seula.boardspring.domain.board.adapter.out.persistence.entity.BoardEntity;
+import kr.seula.boardspring.domain.board.application.domain.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    void save();
 }
