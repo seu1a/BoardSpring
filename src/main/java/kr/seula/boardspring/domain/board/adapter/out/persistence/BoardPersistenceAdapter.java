07becbfbca44ef7763d272987da2da7f6016bf7b
이슬아 <rheeseula@gmail.com>
2024-03-14 14:52:18 +0900

    Feat :: BoardPersistenceAdapter saveBoard 메서드 구현 (Adapter - Out - Persistence)

--- src/main/java/kr/seula/boardspring/domain/board/adapter/out/persistence/BoardPersistenceAdapter.java
+++ src/main/java/kr/seula/boardspring/domain/board/adapter/out/persistence/BoardPersistenceAdapter.java
@@ -1,9 +1,23 @@
 package kr.seula.boardspring.domain.board.adapter.out.persistence;
 
-import kr.seula.boardspring.domain.board.application.port.out.CreateBoardPort;
+import kr.seula.boardspring.domain.board.adapter.out.persistence.mapper.BoardMapper;
+import kr.seula.boardspring.domain.board.adapter.out.persistence.repository.BoardRepository;
+import kr.seula.boardspring.domain.board.application.domain.model.Board;
+import kr.seula.boardspring.domain.board.application.port.out.SaveBoardPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
-public class BoardPersistenceAdapter implements CreateBoardPort {
+@Component
+@RequiredArgsConstructor
+public class BoardPersistenceAdapter implements SaveBoardPort {
 
+    private BoardRepository boardRepository;
+    private BoardMapper boardMapper;
 
-
+    @Override
+    public void saveBoard(Board board) {
+        boardRepository.save(
+                boardMapper.returnBoardEntityWithoutId(board)
+        );
+    }
 }
