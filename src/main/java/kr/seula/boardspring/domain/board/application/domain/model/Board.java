3614abd3f2b7aff59f474d92d6a1d4e50b109998
이슬아 <rheeseula@gmail.com>
2024-03-15 10:25:32 +0900

    Fix :: Board 도메인 모델 수정 (Application - Domain - Model)

--- src/main/java/kr/seula/boardspring/domain/board/application/domain/model/Board.java
+++ src/main/java/kr/seula/boardspring/domain/board/application/domain/model/Board.java
@@ -1,11 +1,14 @@
 package kr.seula.boardspring.domain.board.application.domain.model;
 
+import kr.seula.boardspring.domain.board.adapter.in.web.dto.CreateBoardDTO;
 import kr.seula.boardspring.domain.board.application.domain.model.value.BoardDescription;
 import kr.seula.boardspring.domain.board.application.domain.model.value.BoardId;
 import kr.seula.boardspring.domain.board.application.domain.model.value.BoardTitle;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Builder
+@AllArgsConstructor
 public class Board {
 
     private BoardId id;
@@ -13,5 +16,13 @@ public class Board {
     private BoardTitle title;
 
     private BoardDescription description;
+
+    public Board createBoard(CreateBoardDTO boardDTO) {
+        return new Board(
+                null,
+                 new BoardTitle(boardDTO.getTitle()),
+                new BoardDescription(boardDTO.getDescription())
+        );
+    }
 
 }
