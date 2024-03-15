f672f833b7610d44b32d1f5806d167cc6d51865b
이슬아 <rheeseula@gmail.com>
2024-03-15 19:15:59 +0900

    Feat :: Board 도메인 컨트롤러 추가 (Adapter - In - Web - Controller)

--- src/main/java/kr/seula/boardspring/domain/board/adapter/in/web/controller/BoardController.java
+++ src/main/java/kr/seula/boardspring/domain/board/adapter/in/web/controller/BoardController.java
@@ -1,10 +1,23 @@
 package kr.seula.boardspring.domain.board.adapter.in.web.controller;
 
+import kr.seula.boardspring.domain.board.adapter.in.web.dto.CreateBoardDTO;
+import kr.seula.boardspring.domain.board.application.port.in.CreateBoardUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("board")
+@RequiredArgsConstructor
 public class BoardController {
+    private final CreateBoardUseCase createBoardUseCase;
 
+    @PostMapping("/create")
+    public String createBoard(
+            @RequestBody CreateBoardDTO createBoardDTO){
+        createBoardUseCase.createBoard(createBoardDTO);
+        return "SUCCESS";
+    }
 }
