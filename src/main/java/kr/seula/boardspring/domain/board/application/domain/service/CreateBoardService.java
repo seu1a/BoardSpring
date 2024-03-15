bf62ce103da172b3735995cf9195a55f66873fb1
이슬아 <rheeseula@gmail.com>
2024-03-15 19:20:57 +0900

    Feat :: Board 도메인 CreateBoardUseCase 서비스 구현 (Application - Domain - Service)

--- src/main/java/kr/seula/boardspring/domain/board/application/domain/service/CreateBoardService.java
+++ src/main/java/kr/seula/boardspring/domain/board/application/domain/service/CreateBoardService.java
@@ -1,4 +1,26 @@
 package kr.seula.boardspring.domain.board.application.domain.service;
 
-public class CreateBoardService {
+import jakarta.transaction.Transactional;
+import kr.seula.boardspring.domain.board.adapter.in.web.dto.CreateBoardDTO;
+import kr.seula.boardspring.domain.board.application.domain.model.Board;
+import kr.seula.boardspring.domain.board.application.port.in.CreateBoardUseCase;
+import kr.seula.boardspring.domain.board.application.port.out.SaveBoardPort;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class CreateBoardService implements CreateBoardUseCase {
+
+    private SaveBoardPort saveBoardPort;
+
+    @Override
+    public void createBoard(CreateBoardDTO boardDTO) {
+        saveBoardPort.saveBoard(
+                Board.createBoard(boardDTO)
+        );
+    }
+
 }
--- src/main/java/kr/seula/boardspring/domain/board/application/port/in/CreateBoardUseCase.java
+++ src/main/java/kr/seula/boardspring/domain/board/application/port/in/CreateBoardUseCase.java
@@ -1,4 +1,9 @@
 package kr.seula.boardspring.domain.board.application.port.in;
 
+import kr.seula.boardspring.domain.board.adapter.in.web.dto.CreateBoardDTO;
+
 public interface CreateBoardUseCase {
+
+    void createBoard(CreateBoardDTO createBoardDTO);
+
 }
