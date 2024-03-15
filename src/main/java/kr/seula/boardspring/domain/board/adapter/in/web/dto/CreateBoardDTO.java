db9e470bbe0a791df6f826e276c4cb1bfaf14b47
이슬아 <rheeseula@gmail.com>
2024-03-15 10:22:36 +0900

    Feat :: Board 도메인 CreateBoard DTO 생성 (Adapter - In - Web - Dto)

---
+++ src/main/java/kr/seula/boardspring/domain/board/adapter/in/web/dto/CreateBoardDTO.java
@@ -0,0 +1,12 @@
+package kr.seula.boardspring.domain.board.adapter.in.web.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateBoardDTO {
+
+    private String title;
+
+    private String description;
+
+}
