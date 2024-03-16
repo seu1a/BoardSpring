f11441a16b7d9c305ba4df26a5cf7a3033ac3578
이슬아 <rheeseula@gmail.com>
2024-03-16 17:06:52 +0900

    Feat :: BoardMapper BoardEntity -> Board Mapper 메서드 추가 (Adapter - Out - Persistence - Mapper)

--- src/main/java/kr/seula/boardspring/domain/board/adapter/out/persistence/mapper/BoardMapper.java
+++ src/main/java/kr/seula/boardspring/domain/board/adapter/out/persistence/mapper/BoardMapper.java
@@ -2,6 +2,9 @@ package kr.seula.boardspring.domain.board.adapter.out.persistence.mapper;
 
 import kr.seula.boardspring.domain.board.adapter.out.persistence.entity.BoardEntity;
 import kr.seula.boardspring.domain.board.application.domain.model.Board;
+import kr.seula.boardspring.domain.board.application.domain.model.value.BoardDescription;
+import kr.seula.boardspring.domain.board.application.domain.model.value.BoardId;
+import kr.seula.boardspring.domain.board.application.domain.model.value.BoardTitle;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +15,14 @@ public class BoardMapper {
                 .id(null)
                 .title(board.getTitle().getValue())
                 .description(board.getDescription().getValue())
+                .build();
+    }
+
+    public Board returnBoardEntityToBoard(BoardEntity board) {
+        return Board.builder()
+                .id(new BoardId(board.getId()))
+                .title(new BoardTitle(board.getTitle()))
+                .description(new BoardDescription(board.getDescription()))
                 .build();
     }
 
