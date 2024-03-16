0b80a444d06adb5708165f7d0d0bcd9b38e9c539
이슬아 <rheeseula@gmail.com>
2024-03-16 17:04:13 +0900

    Fix :: LoadBoardPort 매개변수 타입 long으로 변경 (Application - Port - Out)

--- src/main/java/kr/seula/boardspring/domain/board/application/port/out/LoadBoardPort.java
+++ src/main/java/kr/seula/boardspring/domain/board/application/port/out/LoadBoardPort.java
@@ -1,10 +1,9 @@
 package kr.seula.boardspring.domain.board.application.port.out;
 
-import kr.seula.boardspring.domain.board.adapter.in.web.dto.LoadBoardDTO;
 import kr.seula.boardspring.domain.board.application.domain.model.Board;
 
 public interface LoadBoardPort {
 
-    Board loadBoard(LoadBoardDTO boardDTO);
+    Board loadBoard(long id);
 
 }
