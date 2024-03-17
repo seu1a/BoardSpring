d9a626c10faf9e1f695a25ca1ab9193a6373d3d5
이슬아 <rheeseula@gmail.com>
2024-03-17 21:04:46 +0900

    Feat :: Board 도메인 EditBoardPort 추가 (Application - Port - Out)

---
+++ src/main/java/kr/seula/boardspring/domain/board/application/port/out/EditBoardPort.java
@@ -0,0 +1,9 @@
+package kr.seula.boardspring.domain.board.application.port.out;
+
+import kr.seula.boardspring.domain.board.application.domain.model.Board;
+
+public interface EditBoardPort {
+
+    void editBoard(Board board);
+
+}
